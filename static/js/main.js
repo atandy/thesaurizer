e2e986d3a75dda1a5cfa1be8fb4bfeb8a278cb02
Alex <atandy@appnexus.com>
2015-07-03 18:49:33 -0400

    add refresh paragraph button. replace word on click working, but needs tuning

--- static/js/main.js
+++ static/js/main.js
@@ -1,31 +1,45 @@
 $(document).ready(function() {
 	//TODO: handle errors for words that don't have a hiden div.
-	$("#original-paragraph span")
-		.click(function() {
-			word = $(this).text();
-			$("ul[name=" + word+ "]").show();
-		});
-		/*
-		.mouseout(function(){
-			$("ul." + word).hide();
-		});
-		*/
-	$("#replacement-words span")
-		.click(function() {
-			word = $(this).text();
-			console.log(word);
-			// ew.
-			parent_word = $(this).parent().parent().attr("name");
+	ORIGINAL_PARAGRAPH =  $("#original-paragraph p").html();
 
-			orig_spans = $("#original-paragraph span");
-			for (i=0; i < orig_spans.length; i++) {
-				//TODO: replace the parent word with the replacement word
-				if (i.text() == parent_word) {
-					$("#original-paragraph span").text(i).replace(word);
-				}
+	if(typeof(Storage) !== "undefined") {
+	    // Code for localStorage/sessionStorage.
+	    localStorage.setItem("original_paragraph", ORIGINAL_PARAGRAPH);
+	} else {
+	    // Sorry! No Web Storage support..
+	}
+
+	
+	$("#original-paragraph span").click(function() {
+		word = $(this).text();
+		$("ul[name=" + word+ "]").show();
+	});
+		
+	$("#replacement-words span").click(function() {
+		replacement_word = $(this).text();
+		//console.log(replacement_word);
+		// ew.
+		parent_word = $(this).parent().parent().attr("name");
+
+
+		orig_spans = $("#original-paragraph span");
+		for (i=0; i < orig_spans.length; i++) {
+			//TODO: replace the parent word with the replacement word
+			if (orig_spans[i].innerHTML == parent_word) {
+				$("#original-paragraph span").eq(i).html(replacement_word);
 			}
+		}		
+	});
+	
+	function refreshParagraph() {
+		$("#original-paragraph").html(localStorage.getItem("original_paragraph"));
+	}
 
-			
-		});
+	$("button#refresh-paragraph").click(function() {
+		refreshParagraph();
+	})
+
 });
 
+/* grab the words and their positions from the original paragraph.
+// log the positions for each word. 
